f82ab11748e2a4be4eff68899446229119c02c73
Marcos Guimaraes <marcosguimaraes@hotmail.com>
2013-07-06 14:52:51 -0300

    Criando projeto Locadora

---
+++ src/com/facisa/teste/Testes.java
@@ -0,0 +1,19 @@
+package com.facisa.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testes {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
